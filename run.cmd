bb80a001fb79312c380f75154c309887199a4e99
JevgeniT <53855590+JevgeniT@users.noreply.github.com>
2023-10-09 21:52:52 +0300

    Delete run.cmd

--- run.cmd
+++
@@ -1,7 +0,0 @@
-echo off
-echo -----"running ...."-----
-dotnet ef database update 0 --project EventApp.Repository --startup-project EventApp
-dotnet ef migrations remove --project EventApp.Repository --startup-project EventApp
-dotnet ef migrations add Init --project EventApp.Repository --startup-project EventApp
-dotnet ef database update --project EventApp.Repository --startup-project EventApp
-echo -----"seems legit"-----
